57b33f38d259ebf01b6ae9e5035ad8e314520ebe
Tye Porter <tyeporterapps@gmail.com>
2022-05-31 02:41:52 -0500

    Modified DSL File

--- job-dsl/node.groovy
+++ job-dsl/node.groovy
@@ -1,4 +1,4 @@
-job('NodeJS example') {
+job('NodeJS Example') {
     scm {
         git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
